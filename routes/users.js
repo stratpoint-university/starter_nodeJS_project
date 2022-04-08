21204a8f7ef2d8d7ce4b20963db2eb1792df8c6f
Stratpoint University <99856200+stratpoint-university@users.noreply.github.com>
2022-04-08 12:46:57 +0800

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+function fetchUserPasswordFromDB(user){
+  return 'password';
+}
+
+function isAuthenticated(user, password){
+  if (password == fetchUserPasswordFromDB(user)){
+    return true;
+  }else{
+    return false;
+  }
+}
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  if (isAuthenticated(req.params.userID, requ.params.password)){
+    res.send('respond with a resource');
+  }
 });
 
 module.exports = router;
